42fe09c092a84f97c173f3dfe944c13bbecb4586
Lunar <103080318+weirdss3@users.noreply.github.com>
2025-10-07 13:41:39 -0500

    Fixed lag maybe idk accept ts  commit if you're niche
    TS SO NICHE

--- Menu/Main.cs
+++ Menu/Main.cs
@@ -163,7 +163,7 @@ namespace GGTemps.Menu
             while (pingcounter)
             {
                 pingText.text = "Ping: " + PhotonNetwork.GetPing().ToString();
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(1f);
             }
         }
 
